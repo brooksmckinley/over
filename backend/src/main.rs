545ce343c0063d9cff99a876d854ec715da176d2
Brooks McKinley <brooksmckinley@yahoo.com>
2022-07-21 15:58:42 -0400

    Backend hello world

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
+extern crate ws;
+
+use ws::listen;
+
 fn main() {
     println!("Hello, world!");
+
+    listen("0.0.0.0:23849", |sender| {
+
+        move |msg| {
+            sender.send("Hello world!")
+        }
+        
+    }).expect("Failed to listen on port 23849.");
+
 }
